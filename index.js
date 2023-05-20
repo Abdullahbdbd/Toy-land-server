2b57bd5e292f1de1caaedf738846750cab49ad56
Abdullahbdbd <abdullahmohammad262000@gmail.com>
2023-05-20 12:44:10 +0600

    Create specific id to view Details

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
@@ -64,6 +64,17 @@ async function run() {
             const cursor = serviceCollection.find();
             const result = await cursor.toArray();
             res.send(result);
+        })
+
+        app.get('/allToy/:id', async(req, res)=>{
+            const id= req.params.id
+            
+            const filter = {_id : new ObjectId(id)}
+        
+            const data = await serviceCollection.findOne(filter)
+        
+            res.send(data)
+        
         })
 
 
